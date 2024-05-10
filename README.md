eb04dfae8a63f3b6a167808d5069b1025b956e81
Zariah <zaripull18@gmail.com>
2024-05-10 00:10:17 -0500

    added to javascript file, generator is working

--- README.md
+++ README.md
@@ -4,7 +4,13 @@
 
 ## Installation
 
+To utilize this password randomizer,
+a user clicks the "Generate Password" button
+and the generator will produce an 8 character randomized password.
+
 ## Usage
+
+This randomizer is great for users with access to senstive information.
 
 ## License
 
--- script.js
+++ script.js
@@ -10,5 +10,17 @@ function writePassword() {
 
 }
 
+// Function to generate password
+function generatePassword() {
+  var passwordLength = 8; // You can prompt the user for a custom length
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  return password;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
